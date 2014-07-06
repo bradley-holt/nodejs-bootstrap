0137e90dcfc87a636ba8a3377f636a8928b00eaa
Bradley Holt <bradley.holt@foundline.com>
2014-07-06 17:04:22 -0400

    Build LESS and JavaScript files.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    less: {
+      production: {
+        options: {
+          paths: [ "bower_components/bootstrap/less" ],
+          cleancss: true
+        },
+        files: {
+          "dist/css/app.min.css": "less/app.less"
+        }
+      }
+    },
+    uglify: {
+      bootstrap: {
+        files: {
+          "dist/js/app.min.js": [
+            "bower_components/bootstrap/js/transition.js",
+            "bower_components/bootstrap/js/alert.js",
+            "bower_components/bootstrap/js/button.js",
+            "bower_components/bootstrap/js/carousel.js",
+            "bower_components/bootstrap/js/collapse.js",
+            "bower_components/bootstrap/js/dropdown.js",
+            "bower_components/bootstrap/js/modal.js",
+            "bower_components/bootstrap/js/tooltip.js",
+            "bower_components/bootstrap/js/popover.js",
+            "bower_components/bootstrap/js/scrollspy.js",
+            "bower_components/bootstrap/js/tab.js",
+            "bower_components/bootstrap/js/affix.js"
+          ]
+        }
+      }
+    },
+    copy: {
+      jquery: {
+        src: "bower_components/jquery/dist/jquery.min.js",
+        dest: "dist/js/jquery.min.js"
+      }
+    },
+  });
+  
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+   
+  grunt.registerTask("default", [ "less", "uglify", "copy" ]);
+
+};

d25a33efd420045ba359aebbef89cae8ac612acd
Bradley Holt <bradley.holt@foundline.com>
2014-07-06 18:25:20 -0400

    Add Grunt watch task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,12 +39,21 @@ module.exports = function(grunt) {
         dest: "dist/js/jquery.min.js"
       }
     },
+    watch: {
+      less: {
+        files: [
+          "less/*.less"
+        ],
+        tasks: [ "less" ]
+      }
+    },
   });
   
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
    
   grunt.registerTask("default", [ "clean", "less", "uglify", "copy" ]);
 

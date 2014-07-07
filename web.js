8ceb36c5c63307b1522793a7d922c92719ea38a7
Bradley Holt <bradley.holt@foundline.com>
2014-07-07 19:59:34 -0400

    Use static file for homepage rather than sending file.

--- web.js
+++ web.js
@@ -5,10 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.use(express.static('dist'));
-
-app.get('/', function(req, res) {
-  res.sendfile('files/index.html');
-});
+app.use(express.static('public'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
